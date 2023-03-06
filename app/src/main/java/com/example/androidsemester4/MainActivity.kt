a86a3f7610824dbadeb905c759d879ae192d3419
Timur <timurka.askarov@inbox.ru>
2023-03-06 18:43:59 +0300

    androidYaml

--- app/src/main/java/com/example/androidsemester4/MainActivity.kt
+++ app/src/main/java/com/example/androidsemester4/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.androidsemester4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidsemester4.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding: ActivityMainBinding?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding=ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
     }
 }
