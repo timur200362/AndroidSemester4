914c6ece3ee53e75cf4d261fb9b583082242bb9f
Timur <timurka.askarov@inbox.ru>
2023-05-07 19:14:18 +0300

    private

--- app/src/main/java/com/example/androidsemester4/domain/GetNearCitiesUseCase.kt
+++ app/src/main/java/com/example/androidsemester4/domain/GetNearCitiesUseCase.kt
@@ -3,7 +3,7 @@ package com.example.androidsemester4.domain
 import com.example.androidsemester4.data.CityRepository
 import com.example.androidsemester4.ui.Model.City
 
-class GetNearCitiesUseCase(val cityRepository: CityRepository) {
+class GetNearCitiesUseCase(private val cityRepository: CityRepository) {
     suspend fun execute(latitude: Double, longitude: Double): List<City> {
         return cityRepository.getNearCity(latitude, longitude)
     }
--- app/src/main/java/com/example/androidsemester4/ui/SearchWeatherViewModel.kt
+++ app/src/main/java/com/example/androidsemester4/ui/SearchWeatherViewModel.kt
@@ -14,7 +14,7 @@ import com.example.androidsemester4.ui.Model.City
 import com.google.android.gms.location.*
 import kotlinx.coroutines.launch
 
-class SearchWeatherViewModel(val application: Application,val getNearCitiesUseCase:GetNearCitiesUseCase) : ViewModel() {
+class SearchWeatherViewModel(val application: Application, private val getNearCitiesUseCase:GetNearCitiesUseCase) : ViewModel() {
 
     private val _isLoading = MutableLiveData<Boolean>(false)
     val isLoading: LiveData<Boolean>
