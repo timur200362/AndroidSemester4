97c213cdbf706c82d654498331691b65cf2d63e8
timur200362 <kh-g-2016@mail.ru>
2023-11-30 00:51:34 +0300

    compose temp

--- app/src/main/java/com/example/androidsemester4/ui/ComposeTest.kt
+++ app/src/main/java/com/example/androidsemester4/ui/ComposeTest.kt
@@ -16,7 +16,7 @@ fun ComposeTest(name:String,state:SecondScreenState) {
     Text("Влажность: ${state.weatherUi.humidity}", color = Color.White,modifier = Modifier.padding(top=15.dp))
     Text("${state.weatherUi.temp}°", color = Color.White, modifier = Modifier.padding(top=30.dp))
     Text("${state.weatherUi.tempMin}°/${state.weatherUi.tempMax}°",color = Color.White, modifier = Modifier.padding(top=45.dp))
-    Text("Ощущается как: ${state.weatherUi.feelsLike}",color = Color.White, modifier = Modifier.padding(top=60.dp))
+    Text("Ощущается как: ${state.weatherUi.feelsLike}°",color = Color.White, modifier = Modifier.padding(top=60.dp))
     Text("${state.weatherUi.description}", color = Color.White,modifier = Modifier.padding(top=75.dp))
     Text("Восход: ${state.weatherUi.sunrise}",color = Color.White, modifier = Modifier.padding(top=90.dp))
     Text("Закат: ${state.weatherUi.sunset}",color = Color.White, modifier = Modifier.padding(top=105.dp))
